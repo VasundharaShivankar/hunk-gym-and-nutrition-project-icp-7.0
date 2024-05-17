db750d43b209f437d064edc3fc9ee6d4690dad48
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-05-17 20:38:09 +0530

    js added to login page

--- js/login.js
+++ js/login.js
@@ -1,3 +1,36 @@
+function validateForm(event) {
+    
+    const firstNameInput =document.getElementById('first');
+    const firstName= firstNameInput.value.trim();
+    if (!firstName) {
+        alert("Please fill out the First Name field.");
+        event.preventDefault();
+    }
+
+    const lastNameInput = document.getElementById('last');
+    const lastName = lastNameInput.value.trim();
+    if (!lastName) {
+        alert("Please fill out the Last Name field.");
+        event.preventDefault();
+    }
+
+    const emailInput = document.getElementById('email');
+    const emailName = emailInput.value.trim();
+    if (!emailName) {
+    alert("Please fill out the email");
+    event.preventDefault();
+    }
+    
+    const phoneNumberInput =document.getElementById('phone');
+    const phoneNumber= phoneNumberInput.value.trim();
+    if (phoneNumber.length!== 10) {
+        alert("Please fill out the 10 digits for phone number");
+        event.preventDefault();
+    }
+    }
+
+
+
 function inc() {
     const quantityText = document.getElementById('quan-txt');
     let quantity = parseInt(quantityText.innerText);
